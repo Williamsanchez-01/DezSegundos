883e81efa8a5222775031e17da6f7cc5f7c1fb35
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-08-11 15:21:40 -0300

    Adiciona Beep

--- Program.cs
+++ Program.cs
@@ -1,36 +1,50 @@
-﻿Console.Clear();
+﻿#pragma warning disable  CA1416
+
+Console.Clear();
 
 Console.Write("Contando até dez:");
 
 Thread.Sleep(1000);
 Console.Write(" 0");
+Console.Beep(2200, 500);
 
 Thread.Sleep(1000);
 Console.Write(" 1");
+Console.Beep(2200, 500);
 
 Thread.Sleep(1000);
 Console.Write(" 2");
+Console.Beep(2200, 500);
 
 Thread.Sleep(1000);
 Console.Write(" 3");
+Console.Beep(2200, 500);
 
 Thread.Sleep(1000);
 Console.Write(" 4");
+Console.Beep(2200, 500);
 
 Thread.Sleep(1000);
 Console.Write(" 5");
+Console.Beep(2200, 500);
 
 Thread.Sleep(1000);
 Console.Write(" 6");
+Console.Beep(2200, 500);
 
 Thread.Sleep(1000);
 Console.Write(" 7");
+Console.Beep(2200, 500);
 
 Thread.Sleep(1000);
 Console.Write(" 8");
+Console.Beep(2200, 500);
+
 
 Thread.Sleep(1000);
 Console.Write(" 9");
+Console.Beep(2200, 500);
 
 Thread.Sleep(1000);
 Console.Write(" 10");
+Console.Beep(2200, 500);
